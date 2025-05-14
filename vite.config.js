e7b4db9cd190a573b5e7cda2a109ae9f30ed773c
BrunoFeitosa5 <bruno.cultura2015@outlook.com>
2025-05-14 16:51:10 -0300

    Fix Vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/',  // Deixe como '/' para Vercel
+  base: '/',  // Caso queira usar '/' para Vercel, ou '/Meu-Portf-lio/' para GitHub Pages
   plugins: [react()],
-})
+});
